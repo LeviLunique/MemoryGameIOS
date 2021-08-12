100fbe8661e8b20d88ca46951f3afb6c3b4d071e
Levi Lunique <levi.lunique@gmail.com>
2021-08-11 21:29:01 -0400

    First Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by user204006 on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
